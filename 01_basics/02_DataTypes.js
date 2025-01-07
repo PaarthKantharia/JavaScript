c0dbb0fd566af513f904c4e8b26b887b7001143b
Paarth Kantharia <paarth.kantharia0078@gmail.com>
2025-01-07 01:09:17 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use Strict" // Treat all JS as newer versions 
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3 
+    + 
+     3) // code readiblity should be high
+
+     console.log("Paarth Kantharia")
+
+     let name = "Paarth Kantharia"
+     let age = 18
+     let isLoggedIn = false
+     let state;
+
+     // number => 2 to power 53
+     // bigint
+     // string => ""
+     // boolean => true/false
+     // null => standalone value
+     // undefined => 
+     // symbol => unique
+
+
+
+     // object
+
+     console.log(typeof undefined); // undefined
+     console.log(typeof null); // object
